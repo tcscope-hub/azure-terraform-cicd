87cea9058230a1bf6d310697f4052842d53b7ab5
tcscope-hub <tcscope724@gmail.com>
2026-03-08 16:13:29 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "canadacentral"
+}
+
+variable "prefix" {
+  type    = string
+  default = "cicd-demo"
+}
+
+variable "vm_admin_username" {
+  type    = string
+  default = "azureuser"
+}

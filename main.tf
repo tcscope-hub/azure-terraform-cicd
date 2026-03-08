c00ae48f68af5bfbb47df443e27accc0d474e5e7
tcscope-hub <tcscope724@gmail.com>
2026-03-08 16:58:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "${var.prefix}-vm"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1s"
+  size                = "Standard_DS1_v2"
   admin_username      = var.vm_admin_username
 
   network_interface_ids = [azurerm_network_interface.nic.id]

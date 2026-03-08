39ac4e7d98e43c7ef9d2606f8af02573d6dfa5b7
tcscope-hub <tcscope724@gmail.com>
2026-03-08 16:13:08 -0400

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  cloud {
+    organization = "Project-CR460"
+    workspaces {
+      name = "azure-terraform-cicd"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
